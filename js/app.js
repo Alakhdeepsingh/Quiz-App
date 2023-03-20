dc31a1f61e5e4d81040196628fdfcd24e22e0029
Alakhdeep singh <54256747+Alakhdeepsingh@users.noreply.github.com>
2023-03-20 19:33:04 +0530

    quiz

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
+// array ke andar object hai ( this is called array of objects )
+//we have written const because we don't want to change data in future or in other scope
 const quizData = [{
         question: "Which of the following is a client site language?",
         a: "Java",
         b: "C",
-        c: "Python",
+        c: "Python", 
         d: "JavaScript",
         correct: "d",
     },
@@ -18,7 +20,7 @@ const quizData = [{
         question: "What year was JavaScript launched?",
         a: "1996",
         b: "1995",
-        c: "1994",
+        c: "1993",
         d: "none of the above",
         correct: "b",
     },
@@ -32,77 +34,42 @@ const quizData = [{
         correct: "b",
     }
 ];
+
 let index = 0;
-let correct = 0,
-    incorrect = 0,
-    total = quizData.length; //4
-    //console.log(total);
-    //4
-let questionBox = document.getElementById("questionBox");
-{/* <h3 id="questionBox"> */}
-    {/* 1) Lorem ipsum dolor sit amet, consectetur adipisicing elit Debitis? */}
-{/* </h3> */}
-let allInputs = document.querySelectorAll("input[type='radio']")
-console.log(allInputs);
-// The querySelectorAll() method returns all elements that matches a CSS selector(s).
-// The querySelectorAll() method returns a NodeList.
-const loadQuestion = () => {
-    if (total === index) { //4==0
-        return quizEnd()
-    }
-    reset()
+
+const loadQuestions = () =>{
     const data = quizData[index]
     console.log(data);
-    questionBox.innerHTML = `${index + 1}) ${data.question}`
-    //for entering using using js in hmtl
-    allInputs[0].nextElementSibling.innerText = data.a
-    allInputs[1].nextElementSibling.innerText = data.b
-    allInputs[2].nextElementSibling.innerText = data.c
-    allInputs[3].nextElementSibling.innerText = data.d
-    //for entering a,b,c,d options using using js in hmtl
 }
 
-document.querySelector("#submit").addEventListener(
-    "click",
-    function() {
-        const data = quizData[index]
-        const ans = getAnswer()
-        if (ans === data.correct) {
-            correct++;
-        } else {
-            incorrect++;
-        }
-        index++;
-        loadQuestion()
-    }
-)
 
-const getAnswer = () => {
-    let ans;
-    allInputs.forEach(
-        (inputEl) => {
-            if (inputEl.checked) {
-                ans = inputEl.value;
-            }
-        }
-    )
-    return ans;
-}
 
-const reset = () => {
-    allInputs.forEach(
-        (inputEl) => {
-            inputEl.checked = false;
-        }
-    )
-}
 
-const quizEnd = () => {
-    // console.log(document.getElementsByClassName("container"));
-    document.getElementsByClassName("container")[0].innerHTML = `
-        <div class="col">
-            <h3 class="w-100"> Hii, you've scored ${correct} / ${total} </h3>
-        </div>
-    `
-}
-loadQuestion(index);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
